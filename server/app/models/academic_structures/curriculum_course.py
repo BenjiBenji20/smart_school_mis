f00d329f6a9bd6dc0d0c0502ab0fa05796fe9f88
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-12-23 15:32:17 +0800

    refactor: made foreign keys unnullable

--- server/app/models/academic_structures/curriculum_course.py
+++ server/app/models/academic_structures/curriculum_course.py
@@ -17,21 +17,23 @@ class CurriculumCourse(Base):
     id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
     
+    # foreign keys
+    curriculum_id = Column(String(36), ForeignKey("curriculum.id"), nullable=False)
+    course_id = Column(String(36), ForeignKey("course.id"), nullable=False)
+    
     year_level = Column(SmallInteger, nullable=False)
     semester = Column(SmallInteger, nullable=False)
     is_required = Column(Boolean, default=True)
 
+    # course can appear only once in a curriculum
+    # and a curriculum can have multiple courses but they should be different
     __table_args__ = (
         UniqueConstraint(
             "curriculum_id", "course_id",
             name="uq_curriculum_course"
         ),
     )
-    
-    # foreign keys
-    curriculum_id = Column(String(36), ForeignKey("curriculum.id"), nullable=True)
-    course_id = Column(String(36), ForeignKey("course.id"), nullable=True)
-    
+
     
     # many-to-one relationship with Curriculum
     curriculum = relationship(
